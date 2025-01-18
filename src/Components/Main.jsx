2d8f4f074fa38f2ba6539899e9d68b04d57a2f32
LordAnthrax94 <andreagi1994@gmail.com>
2025-01-18 15:12:45 +0100

    fix: impostato bootstrap e inserito form

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 const Main = ()=>{
   return (
    <main>
-     
+    <div className="container">
+      <div className="input-group mb-3 mt-5">
+        <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+        <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+      </div>
+    </div>
+      
+
    </main>
   ) 
  };
